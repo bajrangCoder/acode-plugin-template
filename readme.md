37ea93106715cf501da3045f9e3d69523185d25d
bajrangCoder <nkray21111983@gmail.com>
2022-12-09 12:44:52 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# AcodePlugin
+> Note: Official Acode plugin template returns many errors. This template is fully updated for termux.
+
+For Documentation -> https://github.com/deadlyjack/acode-plugin
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import plugin from '../plugin.json';
+
+class AcodePlugin {
+
+    async init() {
+        
+    }
+
+    async destroy() {
+        
+    }
+}
+
+if (window.acode) {
+    const acodePlugin = new AcodePlugin();
+    acode.setPluginInit(plugin.id, (baseUrl, $page, {
+        cacheFileUrl, cacheFile
+    }) => {
+        if (!baseUrl.endsWith('/')) {
+            baseUrl += '/';
+        }
+        acodePlugin.baseUrl = baseUrl;
+        acodePlugin.init($page, cacheFile, cacheFileUrl);
+    });
+    acode.setPluginUnmount(plugin.id, () => {
+        acodePlugin.destroy();
+    });
+}
